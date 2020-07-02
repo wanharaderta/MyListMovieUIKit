175765bd9a8043f024fca068d34050579c9e3462
wanhar <wanhar@sehatq.com>
2020-07-02 22:28:06 +0700

    Initial Commit

---
+++ MyListMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyListMovie
+//
+//  Created by Wanhar on 02/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
